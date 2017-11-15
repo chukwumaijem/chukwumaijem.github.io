ab6f8e8bd644e90030ffbdcb43cff86aaa6a17ac
Chukwuma Ezumezu <chukwuma.ezumezu@andel.com>
2017-11-15 20:29:48 +0100

    add footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const Footer = () =>
-  <div>
-    Footer
+  <div style={{display: 'flex', justifyContent: 'space-around', paddingTop: '10px', paddingBottom: '10px', backgroundColor: '#f8f8f8'}}>
+    <span>&copy; 2016 Chukwuma Ezumezu</span>
+    <span>
+      <a target="_blank" href="https://themewagon.com/themes/developer-a-responsive-personal-website-template/">
+        Based on ThemeWagon's Design
+      </a>
+    </span>
   </div>;
 
 export default Footer;
