b34005a7f90d206455f3b650cef658b34940ed22
Chukwuma Ezumezu <chukwuma.ezumezu@andel.com>
2017-11-16 14:45:22 +0100

    edit footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () =>
-  <div style={{display: 'flex', justifyContent: 'space-around', paddingTop: '10px', paddingBottom: '10px', backgroundColor: '#f8f8f8'}}>
+  <div className="footer">
     <span>&copy; 2016 Chukwuma Ezumezu</span>
     <span>
       <a target="_blank" href="https://themewagon.com/themes/developer-a-responsive-personal-website-template/">
