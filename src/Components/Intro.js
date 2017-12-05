a58e9a3cd7e8b53d04fbee7f2193689a2a8abb08
Chukwuma Ezumezu <chukwuma.ezumezu@andel.com>
2017-12-05 16:47:31 +0100

    dim the intro bg image

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Intro = () =>
   <div id="home" className="intro-image">
-    <span className="dev-name">Chukwuma Ezumezu</span>
-    <span className="dev-detail">Fullstack JavaScript Developer.</span>
-    <span className="dev-detail">Sapere aude...</span>
+    <div className="intro-dim">
+      <span className="dev-name">Chukwuma Ezumezu</span>
+      <span className="dev-detail">Fullstack JavaScript Developer.</span>
+      <span className="dev-detail">Sapere aude...</span>
+    </div>
   </div>;
 
 export default Intro;
