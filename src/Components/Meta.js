f7a95034cdc8b0fccfcbcab756b89821ac93184b
Chukwuma Ezumezu <chukwuma.ezumezu@andel.com>
2017-12-09 00:41:53 +0100

    fix comment issue

--- src/Components/Meta.js
+++ src/Components/Meta.js
@@ -5,11 +5,11 @@ import favicon from '../images/favicon.png';
 
 const Meta = () =>
   <Helmet>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
+    /** Global site tag (gtag.js) - Google Analytics */
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-110954268-1"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
+      function gtag(){dataLayer.push(arguments)}
       gtag('js', new Date());
 
       gtag('config', 'UA-110954268-1');
