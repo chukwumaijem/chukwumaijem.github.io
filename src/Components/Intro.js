43a264b794302b05b8e6f73643ad524678ff737e
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2018-06-18 20:49:39 +0100

    update intro component

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react'
 
 const Intro = () =>
-  <div id="home" className="intro-image">
-    <div className="intro-dim">
-      <span className="dev-name">Chukwuma Ezumezu</span>
-      <span className="dev-detail">Fullstack JavaScript Developer.</span>
-      <span className="dev-detail">Sapere aude...</span>
+  <Container fluid>
+    <div id="home" className="intro-image">
+      <div className="intro-dim">
+        <div className="dev-name">Chukwuma Ezumezu</div>
+        <p className="dev-detail">
+          FullStack JavaScript developer. My core stack is PREN (Postgres, React, Express, Node), but my experience doesn't end there. I've worked with NoSQL databases(MongoDB, RethinkDb, firebase), some frontend frameworks(Meteor, Angular), and a few other languages(Swift, Python).
+        </p>
+      </div>
     </div>
-  </div>;
+  </Container>;
 
 export default Intro;
